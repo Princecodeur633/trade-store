04788fc75872454e7df0971a20bb067690044956
princecodeur633 <exaucegloirekassy@gmail.com>
2025-08-28 12:02:11 +0100

    j'ai ajouter un fichier billBatchController.ts

---
+++ src/controllers/customerController.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from "express";
+import { Customer } from "../models/Customer";
+
+export const createCustomer = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const customer = await Customer.create(req.body);
+    res.status(201).json({ success: true, data: customer });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCustomers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const customers = await Customer.findAll();
+    res.json({ success: true, data: customers });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCustomerById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const customer = await Customer.findByPk(req.params.id);
+    if (!customer) return res.status(404).json({ success: false, message: "Customer not found" });
+    res.json({ success: true, data: customer });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateCustomer = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const customer = await Customer.findByPk(req.params.id);
+    if (!customer) return res.status(404).json({ success: false, message: "Customer not found" });
+
+    await customer.update(req.body);
+    res.json({ success: true, data: customer });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteCustomer = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const customer = await Customer.findByPk(req.params.id);
+    if (!customer) return res.status(404).json({ success: false, message: "Customer not found" });
+
+    await customer.destroy();
+    res.json({ success: true, message: "Customer deleted" });
+  } catch (err) {
+    next(err);
+  }
+};
